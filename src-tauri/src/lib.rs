446c9ea1d57208e92bee698168415dc9c3c3dc61
ferlisifrank-lgtm <ferlisi.frank@gmail.com>
2026-01-05 23:26:30 -0500

    windows build fixes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,5 @@
 mod actions;
-#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+// #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 // mod apple_intelligence;
 mod audio_feedback;
 pub mod audio_toolkit;
